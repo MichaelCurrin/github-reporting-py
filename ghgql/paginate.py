5fc2c15e8d49fa00b084c55e66f19705fca61bf5
Michael Currin <michael.currin@gmail.com>
2019-06-11 23:40:17 +0200

    feature: Add paginate.py and first paginate query

---
+++ ghgql/paginate.py
@@ -0,0 +1,31 @@
+"""
+Query with pagination application.
+"""
+import sys
+
+import requests
+
+import config
+import lib
+
+
+def main():
+    path = 'queries/paged/commits.gql'
+    variables = {}
+    for i in range(5):
+        print(f"Query #{i+1}")
+        data = lib.query_by_filename(path, variables=variables)
+        print(lib.prettify(data))
+        page_info = data['repository']['defaultBranchRef']['target']['history']['pageInfo']
+        has_next_page = page_info['hasNextPage']
+        if not has_next_page:
+            print("No more pages")
+            break
+        after = page_info['endCursor']
+        variables['after'] = after
+        print(f"Next page starts after: '{after}'")
+        print()
+
+
+if __name__ == '__main__':
+    main()
