d1a32969e62975adfbaf7ff6903e3c1e4d55f4c2
Michael Currin <michael.currin@gmail.com>
2019-06-11 23:48:51 +0200

    feature: Update paginate.py

--- ghgql/paginate.py
+++ ghgql/paginate.py
@@ -13,8 +13,13 @@ def main():
     for i in range(5):
         print(f"Query #{i+1}")
         data = lib.query_by_filename(path, variables=variables)
-        print(lib.prettify(data))
-        page_info = data['repository']['defaultBranchRef']['target']['history']['pageInfo']
+        history = data['repository']['defaultBranchRef']['target']['history']
+
+        messages = [edge['node']['message'] for edge in history['edges']]
+        for m in messages:
+            print(f"  {m}")
+
+        page_info = history['pageInfo']
         has_next_page = page_info['hasNextPage']
         if not has_next_page:
             print("No more pages")
