9acbfe19c1faa20953f8d09fa5b602eabbf837d9
Michael Currin <michael.currin@gmail.com>
2019-07-01 22:51:39 +0200

    feat: Add about repos logic

---
+++ bin/about_repos.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python ghgql/about_repos.py login 2uinc  > ghgql/var/about.txt
---
+++ ghgql/about_repos.py
@@ -0,0 +1,56 @@
+"""
+About repos application.
+
+Print results for the about repos query for a given login.
+"""
+import sys
+
+import lib
+
+
+def main(args):
+    """
+    Main command-line function.
+    """
+    if len(args) != 2 or set(args).intersection({'-h', '--help'}):
+        print(f"Usage: {__file__} login LOGIN")
+        sys.exit(1)
+
+    path = 'queries/paged/about_repos.gql'
+    variables = lib.process_variables(args)
+
+    query_counter = 0
+    while True:
+        query_counter += 1
+        print(f"Query #{query_counter}")
+        data = lib.query_by_filename(path, variables)
+
+        repositories = data['repositoryOwner']['repositories']
+        if query_counter == 1:
+            print(f"Total count: {repositories['totalCount']}")
+
+        for node in repositories['nodes']:
+            name = node['name']
+            description = node['description'] or "N/A"
+            created_at = node['createdAt'][:10]
+            updated_at = node['updatedAt'][:10]
+            primary_lang = (node['primaryLanguage'] or {}).get('name', "N/A")
+            langs = [x['name'] for x in node['languages']['nodes']]
+            langs_txt = ", ".join(langs) if langs else "N/A"
+            print(f"Name: {name}")
+            print(f"Description: {description}")
+            print(f"Updated: {updated_at}")
+            print(f"Created: {created_at}")
+            print(f"Primary language: {primary_lang}")
+            print(f"Languages: {langs_txt}")
+            print()
+
+        repo_page_info = repositories['pageInfo']
+        if repo_page_info['hasNextPage']:
+            variables['after'] = repo_page_info['endCursor']
+        else:
+            break
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
