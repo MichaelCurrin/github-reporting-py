5c1af81b5c7a52db6bf7d263dc2015181f4c6f28
Michael Currin <michael.currin@gmail.com>
2019-09-25 14:26:46 +0200

    feat: Add to repo_commits script and query

--- ghgql/repo_commits.py
+++ ghgql/repo_commits.py
@@ -1,26 +1,49 @@
 """
 Repo commits application.
+
+Fetch commits for a given repo and optional start date.
 """
 import sys
 
 import lib
+import lib.git
+
+QUERY_PATH = 'queries/repos/repo_commits.gql'
 
 
-def get_commits(path, variables):
-    # TODO Only need to print or retrieve branch name on first query.
-    # TODO Add paging
-    # TODO Add return
-    resp_data = lib.query_by_filename(path, variables)
-    branch = resp_data['repository']['defaultBranchRef']
-    branch_name = branch['name']
+def process_commits(raw_commits, name, branch_name):
+    output_data = []
+    for c in raw_commits:
+        parsed_commit_data = lib.git.parse_commit(c)
+        out_commit = dict(
+            repo_name=name,
+            branch_name=branch_name,
+            **parsed_commit_data,
+        )
+        output_data.append(out_commit)
+
+    return output_data
+
+
+def get_commits(variables):
+    # TODO: Validte for specific keys.
+    name = variables['name']
+
+    resp = lib.query_by_filename(QUERY_PATH, variables)
+    repo_data = resp['repository']
+
+    branch = repo_data['defaultBranchRef']
+    branch_name = branch.get('name')
+
     commit_history = branch['target']['history']
-    total = commit_history['totalCount']
-    commits = commit_history['nodes']
-    print(branch_name)
-    print(total)
+    total_count = commit_history['totalCount']
 
-    for c in commits:
-        print(c)
+    raw_commits = branch['target']['history']['nodes']
+    commits = process_commits(raw_commits, name, branch_name)
+
+    page_info = repo_data['defaultBranchRef']['target']['history']['pageInfo']
+
+    return commits
 
 
 def main(args):
@@ -28,13 +51,13 @@ def main(args):
     Main command-line function.
     """
     if not args or set(args).intersection({'-h', '--help'}):
-        print(f"Usage: {__file__} owner OWNER name REPO_NAME [start START_DATE]")
+        print(
+            f"Usage: {__file__} owner OWNER name REPO_NAME [start START_DATE]")
         print(f"START_DATE: Count commits on or after this date, in YYYY-MM-DD format.")
         sys.exit(1)
 
-    path = 'queries/repos/repo_commits.gql'
     variables = lib.process_variables(args)
-    get_commits(path, variables)
+    print(get_commits(variables))
 
 
 if __name__ == '__main__':
