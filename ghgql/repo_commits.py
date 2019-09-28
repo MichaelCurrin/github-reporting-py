e19027a37add42301a502a9bef66765563ed3f3a
Michael Currin <michael.currin@gmail.com>
2019-09-28 11:53:57 +0200

    refactor: Create process_response function

--- ghgql/repo_commits.py
+++ ghgql/repo_commits.py
@@ -16,52 +16,62 @@ QUERY_PATH = 'queries/repos/repo_commits.gql'
 CSV_OUT_NAME = 'repo-commits--{repo_name}--end-{end_date}--start-{start_date}.csv'
 
 
+def process_response(resp, repo_name):
+    """
+    Format response for a request of repo commits
+    """
+    repo_data = resp['repository']
+
+    branch = repo_data['defaultBranchRef']
+    branch_name = branch.get('name')
+
+    commit_history = branch['target']['history']
+    total_commits = commit_history['totalCount']
+
+    raw_commits = branch['target']['history']['nodes']
+    commits = [lib.git.prepare_row(c, repo_name, branch_name)
+               for c in raw_commits]
+
+    page_info = commit_history['pageInfo']
+    cursor = page_info['endCursor'] if page_info['hasNextPage'] else None
+
+    return commits, total_commits, cursor
+
+
 def get_commits(owner, repo_name, start_date=None):
     """
     Fetch all commits for a given repo and optional start date.
 
-    Uses paging if there is more than 1 page of 100 commits to fetch.
-    Returns a list of zero or more dict objects with commit data.
+    Uses paging if there is more than 1 page of 100 commits to fetch. Returns a
+    list of zero or more dict objects with commit data.
     """
-    results = []
-
-    branch_name = None
     since = lib.time.timestamp(start_date) if start_date else None
 
-    variables = dict(
+    query_variables = dict(
         owner=owner,
         repo_name=repo_name,
         since=since,
     )
 
+    results = []
+
     counter = 0
     while True:
         counter += 1
 
-        resp = lib.query_by_filename(QUERY_PATH, variables)
+        resp = lib.query_by_filename(QUERY_PATH, query_variables)
+        commits, total_commits, cursor = process_response(resp, repo_name)
 
-        repo_data = resp['repository']
+        if counter == 1:
+            print("Summary")
+            print(f" - commits: {total_commits}")
+            print(f" - pages: {math.ceil(total_commits / 100)}")
 
-        branch = repo_data['defaultBranchRef']
-        if not branch_name:
-            branch_name = branch.get('name')
-
-        commit_history = branch['target']['history']
-        if not variables.get('cursor', None):
-            print("Expected")
-            print(f" - commits: {commit_history['totalCount']}")
-            print(f" - pages: {math.ceil(commit_history['totalCount'] / 100)}")
-
-        raw_commits = branch['target']['history']['nodes']
-        commits = [lib.git.prepare_row(c, repo_name, branch_name) for c in
-                   raw_commits]
         results.extend(commits)
-
         print(f"Processed page: #{counter}")
 
-        page_info = commit_history['pageInfo']
-        if page_info['hasNextPage']:
-            variables['cursor'] = page_info['endCursor']
+        if cursor:
+            query_variables['cursor'] = cursor
         else:
             break
 
@@ -92,24 +102,24 @@ def main():
 
     parser.add_argument(
         'owner',
-        metavar="OWNER"
+        metavar="OWNER",
     )
     parser.add_argument(
         'repo_name',
-        metavar="REPO_NAME"
+        metavar="REPO_NAME",
     )
     parser.add_argument(
         '-s', '--start',
         metavar="DATE",
         help="Optionally filter to commits from this date onwards."
-            " Format: 'YYYY-MM-DD'."
+            " Format: 'YYYY-MM-DD'.",
     )
 
     args = parser.parse_args()
     commits_to_csv(
         args.owner,
         args.repo_name,
-        args.start
+        args.start,
     )
 
 
