dc0d550496418be28fd1df7511f432d9105fb6a7
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-10-02 12:32:07 +0200

    Update time.py

--- ghgql/lib/time.py
+++ ghgql/lib/time.py
@@ -6,24 +6,25 @@ import datetime
 
 def as_git_timestamp(value: [datetime.date, datetime.datetime, str]) -> str:
     """
-    Convert date to GitTimestamp string.
+    Convert date to ISO timestamp string.
 
-    The output format is the datetime formatted string required by Github's
-    GraphQL. Any time in the input value is dropped so the result is for
-    midnight.
+    Output as an ISO datetime format string, as required by the GitHub
+    GraphQL schema's `GitTimestamp` object. 
+    
+    Any time in the input value gets dropped, such that the time is at midnight.
     """
     dt = datetime.datetime.strptime(str(value), "%Y-%m-%d")
 
     return dt.isoformat()
 
 
-def as_date(datetime_str: str) -> datetime.date:
+def as_date(dt: str) -> datetime.date:
     """
     Convert string which starts with "YYYY-MM-DD" to a date object.
     """
-    date_str = datetime_str[:10]
+    date = dt[:10]
 
-    return datetime.datetime.strptime(date_str, "%Y-%m-%d").date()
+    return datetime.datetime.strptime(date, "%Y-%m-%d").date()
 
 
 def days_ago(days: int) -> datetime.date:
