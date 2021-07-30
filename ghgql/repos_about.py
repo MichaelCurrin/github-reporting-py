cbf71a5e486cc46743604f02409d2d195803495a
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-07-30 13:08:47 +0200

    refactor: update repos_about.py

--- ghgql/repos_about.py
+++ ghgql/repos_about.py
@@ -13,35 +13,46 @@ import lib.text
 QUERY_PATH = "queries/repos/repos_about.gql"
 
 
+def process(repo_node):
+    name = repo_node["name"]
+    description = repo_node["description"] or "N/A"
+
+    created_at = repo_node["createdAt"][:10]
+    updated_at = repo_node["updatedAt"][:10]
+
+    primary_lang = (repo_node["primaryLanguage"] or {}).get("name", "N/A")
+    langs = [x["name"] for x in repo_node["languages"]["nodes"]]
+    langs_txt = ", ".join(langs) if langs else "N/A"
+
+    print(f"Name: {name}")
+    print(f"Description: {description}")
+    print(f"Updated: {updated_at}")
+    print(f"Created: {created_at}")
+    print(f"Primary language: {primary_lang}")
+    print(f"Languages: {langs_txt}")
+    print()
+
+
 def report(path, variables):
     query_counter = 0
 
     while True:
         query_counter += 1
         print(f"Query #{query_counter}")
+
         data = lib.query_by_filename(path, variables)
-
         repositories = data["repositoryOwner"]["repositories"]
-        if query_counter == 1:
-            print(f"Total count: {repositories['totalCount']}")
 
-        for node in repositories["nodes"]:
-            name = node["name"]
-            description = node["description"] or "N/A"
-            created_at = node["createdAt"][:10]
-            updated_at = node["updatedAt"][:10]
-            primary_lang = (node["primaryLanguage"] or {}).get("name", "N/A")
-            langs = [x["name"] for x in node["languages"]["nodes"]]
-            langs_txt = ", ".join(langs) if langs else "N/A"
-            print(f"Name: {name}")
-            print(f"Description: {description}")
-            print(f"Updated: {updated_at}")
-            print(f"Created: {created_at}")
-            print(f"Primary language: {primary_lang}")
-            print(f"Languages: {langs_txt}")
-            print()
-
+        repo_nodes = repositories["nodes"]
+        total = repositories["totalCount"]
         repo_page_info = repositories["pageInfo"]
+
+        if query_counter == 1:
+            print(f"Total count: {total}")
+
+        for repo_node in repo_nodes:
+            process(repo_node)
+
         if repo_page_info["hasNextPage"]:
             variables["cursor"] = repo_page_info["endCursor"]
         else:
