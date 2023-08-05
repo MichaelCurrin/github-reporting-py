946fbb2c8be6651ae3158ba33ce39d73a82e3aa5
Quantifier <quantifier-tech@protonmail.com>
2023-08-05 12:44:32 -0400

    (commit) Add verbosity, end date and committer filters

--- ghgql/repo_commits.py
+++ ghgql/repo_commits.py
@@ -3,7 +3,7 @@
 Repo commits report application.
 
 Fetch all commits for a single given repo using paging. Accepts an optional
-start date.
+committer, output dir, start date, end date, and verbosity.
 """
 import argparse
 import datetime
@@ -14,7 +14,7 @@ import lib
 import lib.git
 
 QUERY_PATH = Path("queries/repos/repo_commits.gql")
-CSV_OUT_NAME = "repo-commits--{repo_name}--end-{end_date}--start-{start_date}.csv"
+CSV_OUT_NAME = "repo-commits--{repo_name}--start-{start_date}--end-{end_date}.csv"
 
 
 def parse(resp: dict):
@@ -22,33 +22,35 @@ def parse(resp: dict):
     Parse response data for the repo commits query.
     """
     branch = resp["repository"]["defaultBranchRef"]
-
     branch_name = branch.get("name")
-
     commit_history = branch["target"]["history"]
     total_commits = commit_history["totalCount"]
     commits = commit_history["nodes"]
     page_info = commit_history["pageInfo"]
-
     cursor = page_info["endCursor"] if page_info["hasNextPage"] else None
 
     return branch_name, total_commits, commits, cursor
 
 
-def process_response(resp: dict, repo_name: str):
+def process_response(resp: dict, repo_name: str, verbose=False):
     """
     Format the response from a request for repo commits.
     """
     branch_name, total_commits, commits, cursor = parse(resp)
-
     processed_commits = [
-        lib.git.prepare_row(c, repo_name, branch_name) for c in commits
+        lib.git.prepare_row(c, repo_name, branch_name, verbose) for c in commits
     ]
 
     return processed_commits, total_commits, cursor
 
 
-def get_commits(owner: str, repo_name: str, start_date=None) -> list[dict]:
+def get_commits(
+        owner: str,
+        repo_name: str,
+        committer=None,
+        start_date=None,
+        end_date=None,
+        verbose=False) -> list[dict]:
     """
     Fetch all commits for a given repo and  an optional start date.
 
@@ -58,6 +60,7 @@ def get_commits(owner: str, repo_name: str, start_date=None) -> list[dict]:
     print("/".join((owner, repo_name)))
 
     since = lib.time.as_git_timestamp(start_date) if start_date else None
+    before = lib.time.as_git_timestamp(end_date) if end_date else None
     query_variables = dict(
         owner=owner,
         repo_name=repo_name,
@@ -71,23 +74,44 @@ def get_commits(owner: str, repo_name: str, start_date=None) -> list[dict]:
         counter += 1
 
         resp = lib.query_by_filename(QUERY_PATH, query_variables)
-        commits, total_commits, cursor = process_response(resp, repo_name)
+        commits, _, cursor = process_response(resp, repo_name, verbose)
         results.extend(commits)
-
-        if counter == 1:
-            print(f" - commits: {total_commits}")
-            print(f" - pages: {math.ceil(total_commits / 100)}")
-        print(f"Processed page: #{counter}")
 
         if cursor:
             query_variables["cursor"] = cursor
         else:
             break
 
+    if committer:
+        results = list(filter(
+            lambda res: res["committer_login"] == committer,
+            results
+        ))
+
+    if before:
+        before = lib.time.as_date(before)
+        results = list(filter(
+            lambda res: res["committed_date"] < before,
+            results
+        ))
+
+    if counter == 1:
+        total_commits = len(results)
+        print(f" - commits: {total_commits}")
+        print(f" - pages: {math.ceil(total_commits / 100)}")
+        print(f"Processed page: #{counter}")
+
     return results
 
 
-def commits_to_csv(owner, repo_name, start_date=None):
+def commits_to_csv(
+        owner,
+        repo_name,
+        committer=None,
+        output_dir=None,
+        start_date="START",
+        end_date=datetime.date.today(),
+        verbose=False):
     """
     Write a CSV of all commits in a repo.
 
@@ -95,12 +119,19 @@ def commits_to_csv(owner, repo_name, start_date=None):
     """
     filename = CSV_OUT_NAME.format(
         repo_name=repo_name,
-        end_date=datetime.date.today(),
-        start_date=start_date if start_date else "INIT",
+        start_date=start_date,
+        end_date=end_date,
     )
-    path = lib.VAR_DIR / filename
-
-    repo_commits = get_commits(owner, repo_name, start_date)
+    output_dir = Path(output_dir) if output_dir else lib.VAR_DIR
+    path = output_dir / filename
+    repo_commits = get_commits(
+        owner,
+        repo_name,
+        committer,
+        start_date,
+        end_date,
+        verbose,
+    )
 
     lib.write_csv(path, repo_commits, append=False)
 
@@ -120,18 +151,44 @@ def main():
         metavar="REPO_NAME",
     )
     parser.add_argument(
+        "committer",
+        metavar="COMMITTER",
+    )
+    parser.add_argument(
+        "-o",
+        "--output-dir",
+        metavar="DIR",
+        help="Directory in which to write the csv file.",
+    )
+    parser.add_argument(
         "-s",
         "--start",
         metavar="DATE",
         help="Optionally filter to commits from this date onwards."
         " Format: 'YYYY-MM-DD'.",
     )
+    parser.add_argument(
+        "-e",
+        "--end",
+        metavar="DATE",
+        help="Optionally filter to commits strictly before this date."
+        " Format: 'YYYY-MM-DD'.",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        help="Verbose commit output.",
+    )
 
     args = parser.parse_args()
     commits_to_csv(
         args.owner,
         args.repo_name,
+        args.committer,
+        args.output_dir,
         args.start,
+        args.end,
+        args.verbose,
     )
 
 
