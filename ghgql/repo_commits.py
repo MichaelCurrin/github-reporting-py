01fe40a9ee870401c6662b47547b26edb35bda78
Quantifier <quantifier-tech@protonmail.com>
2023-09-09 14:38:50 -0400

    (commit) Add per branch commit reporting

--- ghgql/repo_commits.py
+++ ghgql/repo_commits.py
@@ -14,14 +14,18 @@ import lib
 import lib.git
 
 QUERY_PATH = Path("queries/repos/repo_commits.gql")
-CSV_OUT_NAME = "repo-commits--{repo_name}--start-{start_date}--end-{end_date}.csv"
+QUERY_PATH_BRANCH = Path("queries/repos/repo_commits_branch.gql")
+
+CSV_OUT_NAME = "{owner}--{repo}--start-{start}--end-{end}.csv"
+CSV_OUT_NAME_BRANCH = "{owner}--{repo}--{branch}--start-{start}--end-{end}.csv"
 
 
-def parse(resp: dict):
+def parse(resp: dict, branch_name):
     """
     Parse response data for the repo commits query.
     """
-    branch = resp["repository"]["defaultBranchRef"]
+    key = "ref" if branch_name else "defaultBranchRef"
+    branch = resp["repository"][key]
     branch_name = branch.get("name")
     commit_history = branch["target"]["history"]
     total_commits = commit_history["totalCount"]
@@ -32,11 +36,11 @@ def parse(resp: dict):
     return branch_name, total_commits, commits, cursor
 
 
-def process_response(resp: dict, repo_name: str, verbose=False):
+def process_response(resp: dict, repo_name: str, branch_name: str, verbose=False):
     """
     Format the response from a request for repo commits.
     """
-    branch_name, total_commits, commits, cursor = parse(resp)
+    branch_name, total_commits, commits, cursor = parse(resp, branch_name)
     processed_commits = [
         lib.git.prepare_row(c, repo_name, branch_name, verbose) for c in commits
     ]
@@ -47,6 +51,7 @@ def process_response(resp: dict, repo_name: str, verbose=False):
 def get_commits(
         owner: str,
         repo_name: str,
+        branch_name=None,
         committer=None,
         start_date=None,
         end_date=None,
@@ -57,13 +62,17 @@ def get_commits(
     Uses paging if there is more than 1 page of 100 commits to fetch. Returns a
     list of zero or more dict objects with commit data.
     """
-    print("/".join((owner, repo_name)))
+    if branch_name:
+        print("/".join((owner, repo_name, branch_name)))
+    else:
+        print("/".join((owner, repo_name)))
 
     since = lib.time.as_git_timestamp(start_date) if start_date else None
     before = lib.time.as_git_timestamp(end_date) if end_date else None
     query_variables = dict(
         owner=owner,
         repo_name=repo_name,
+        branch_name=branch_name,
         since=since,
     )
 
@@ -73,8 +82,9 @@ def get_commits(
     while True:
         counter += 1
 
-        resp = lib.query_by_filename(QUERY_PATH, query_variables)
-        commits, _, cursor = process_response(resp, repo_name, verbose)
+        query_path = QUERY_PATH_BRANCH if branch_name else QUERY_PATH
+        resp = lib.query_by_filename(query_path, query_variables)
+        commits, _, cursor = process_response(resp, repo_name, branch_name, verbose)
         results.extend(commits)
 
         if cursor:
@@ -107,6 +117,7 @@ def get_commits(
 def commits_to_csv(
         owner,
         repo_name,
+        branch_name=None,
         committer=None,
         output_dir=None,
         start_date="START",
@@ -117,16 +128,20 @@ def commits_to_csv(
 
     Existing file will be overwritten.
     """
-    filename = CSV_OUT_NAME.format(
-        repo_name=repo_name,
-        start_date=start_date,
-        end_date=end_date,
+    filename = CSV_OUT_NAME_BRANCH if branch_name else CSV_OUT_NAME
+    filename = filename.format(
+        owner=owner,
+        repo=repo_name,
+        branch=branch_name,
+        start=start_date,
+        end=end_date,
     )
     output_dir = Path(output_dir) if output_dir else lib.VAR_DIR
     path = output_dir / filename
     repo_commits = get_commits(
         owner,
         repo_name,
+        branch_name,
         committer,
         start_date,
         end_date,
@@ -145,14 +160,23 @@ def main():
     parser.add_argument(
         "owner",
         metavar="OWNER",
+        help="Owner of the repository."
     )
     parser.add_argument(
-        "repo_name",
-        metavar="REPO_NAME",
+        "repo",
+        metavar="REPO",
+        help="Repository name."
     )
     parser.add_argument(
         "committer",
         metavar="COMMITTER",
+        help="Username of the commit author."
+    )
+    parser.add_argument(
+        "-b",
+        "--branch",
+        metavar="BRANCH",
+        help="Specific branch to pull commits from.",
     )
     parser.add_argument(
         "-o",
@@ -183,7 +207,8 @@ def main():
     args = parser.parse_args()
     commits_to_csv(
         args.owner,
-        args.repo_name,
+        args.repo,
+        args.branch,
         args.committer,
         args.output_dir,
         args.start,
